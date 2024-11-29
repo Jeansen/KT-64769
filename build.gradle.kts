8908e66e562a76c21f14adce2dc7d9c8e9cae3ec
Marcel Lautenbach <mlautenb@gmail.com>
2024-11-29 18:35:44 +0100

    Bump Kotlin to 2.0.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
-import org.jetbrains.kotlin.config.AnalysisFlags.optIn
 
 plugins {
-    kotlin("multiplatform") version "1.9.22"
-    kotlin("plugin.serialization") version "1.9.22"
+    kotlin("multiplatform") version "2.0.21"
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 group = "me.kotlin"
